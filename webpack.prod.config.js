c7f762eab3fa96d6505acf6121e3eedce3a9fedb
Gabriel Ferreira <gabrielferreira.95@hotmail.com>
2017-01-31 18:03:33 -0200

    gerando bundle de producao

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -57,5 +57,13 @@ module.exports = validate({
       include: /src/,
       loader: extractTextPlugin.extract('style', 'css?modules')
     }]
+  },
+  
+  resolve: {
+    alias: {
+      src: path.join(__dirname, 'src'),
+      components: path.join(__dirname, 'src', 'components'),
+      containers: path.join(__dirname, 'src', 'containers')
+    }
   }
 });
