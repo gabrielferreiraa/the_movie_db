259bc236b0b825a35dee9300c8ef0ba65cf35a73
Gabriel Ferreira <gabriel.alves@itlean.com.br>
2017-06-13 22:26:48 -0300

    ajustes

--- src/reducers/SearchMovieReducer.js
+++ src/reducers/SearchMovieReducer.js
@@ -4,12 +4,12 @@ import { handleActions } from 'redux-actions';
 import { NAME_CHANGED, MOVIE_SEARCHED } from 'constants/SearchMovieConstants';
 
 const INITIAL_STATE = {
-  name: 'Breaking Bad', list: []
+  name: 'Breaking Bad', list: [], isFetching: false
 };
 
 const handlers = {
   [NAME_CHANGED]: (state, action) => ({ ...state, name: action.payload }),
-  [MOVIE_SEARCHED]: (state, action) => ({ ...state, list: action.payload.data })
+  [MOVIE_SEARCHED]: (state, action) => ({ ...state, list: action.payload.data, isFetching: true })
 };
 
 export default handleActions(handlers, INITIAL_STATE);
