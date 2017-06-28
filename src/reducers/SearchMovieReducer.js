f893df5d90fe1839260047e8055ffe8c3bd2fa7c
Gabriel Ferreira <gabriel.alves@itlean.com.br>
2017-06-28 17:52:33 -0300

    mudando reducers

--- src/reducers/SearchMovieReducer.js
+++ src/reducers/SearchMovieReducer.js
@@ -1,15 +1,15 @@
 'use strict';
 
 import { handleActions } from 'redux-actions';
-import { NAME_CHANGED, MOVIE_SEARCHED, MOVIE_DETAIL, CLOSED_ASIDE } from 'constants/SearchMovieConstants';
+import * as types from 'constants/SearchMovieConstants';
 
 const INITIAL_STATE = { name: 'Avengers', list: [], currentMovie: [], open: false };
 
 const handlers = {
-  [NAME_CHANGED]: (state, action) => ({ ...state, name: action.payload, open: false }),
-  [MOVIE_SEARCHED]: (state, action) => ({ ...state, list: action.payload.data, open: false }),
-  [MOVIE_DETAIL]: (state, action) => ({ ...state, currentMovie: action.payload.data, open: true }),
-  [CLOSED_ASIDE]: (state, action) => ({ ...state, open: action.payload })
+  [types.NAME_CHANGED]: (state, action) => ({ ...state, name: action.payload, open: false }),
+  [types.MOVIE_SEARCHED]: (state, action) => ({ ...state, list: action.payload.data, open: false }),
+  [types.MOVIE_DETAIL]: (state, action) => ({ ...state, currentMovie: action.payload.data, open: true }),
+  [types.CLOSED_ASIDE]: (state, action) => ({ ...state, open: action.payload })
 };
 
 export default handleActions(handlers, INITIAL_STATE);
